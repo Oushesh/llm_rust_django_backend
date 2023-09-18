ccfadd395cd7c744ee4feeb4d927c7b22ebd52f4
oushesh <oushesh@gmail.com>
2023-09-18 19:30:35 +0400

    backup

---
+++ hallucination_app/test_nemo_guardrails.py
@@ -0,0 +1,15 @@
+#https://www.youtube.com/watch?v=SwqusllMCnE
+
+
+"""
+Gartner Predicts Chabots will become a
+Primary Customer Service Channel within
+5 years.  Chatbot Investment on the Rise
+but low ROI & other Challenges persist.
+
+By 2027, chatbots will become  the primary
+customer service channel for roughly a
+quarter of organizations, according to
+Gartner Inc.
+
+"""
