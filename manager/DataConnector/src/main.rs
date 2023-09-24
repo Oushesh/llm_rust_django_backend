6b34284bf640e0912972967aa1ae21eb5bec9738
oushesh <oushesh@gmail.com>
2023-09-24 21:01:49 +0400

    main.rs

--- manager/DataConnector/src/main.rs
+++ manager/DataConnector/src/main.rs
@@ -7,12 +7,29 @@ async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn upload_file() -> tide::Result {
+
+}
+
+
 //The macros of shuttle_runtime main allows
 //you to run async fn axum as main.
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .nest_service("/assets", ServeDir::new(PathBuf::from("assets")));
+        .route("/upload",post(upload_file))
+        .nest_service("/assets", ServeDir::new(PathBuf::from("assets")))
+        .layer(tide::middleware::StateMiddleware::new(model))
+        .layer(tide::middleware::StateMiddleware::new(pinecone))
+        .layer(tide::middleware::StateMiddleware::new(qdrant));
+
+    let port = "127.0.0.1:8080";
+    let addr = port.parse()?;
+
+    axum::Server::bind(&addr)
+        .serve(app.into_)
+
+        ;
     Ok(router.into())
 }
