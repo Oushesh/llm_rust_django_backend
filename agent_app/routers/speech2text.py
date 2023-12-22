d49833a2553a5d5829b0af83057b57a9768789ba
oushesh <oushesh@gmail.com>
2023-12-22 20:28:56 +0400

    updated: agent_app/routers/speech2text.py

--- agent_app/routers/speech2text.py
+++ agent_app/routers/speech2text.py
@@ -4,11 +4,17 @@ text prompt from the user and
 calls a specific function
 which will perform a task.
 OpenAI Endpoint decides on how
-
 """
 
+#Current state: Data on GCP Bucket
 
-#Current state: Data on aws:
+import os
+from ninja import Router
+import subprocess
+router = Router()
+
+
+
 
 
 
